fef19e79656e453fd3dcd5d98d85e9b3bafc96a6
arcademakecodesave <137505074+arcademakecodesave@users.noreply.github.com>
2023-06-23 18:40:45 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+	
+})
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . 5 5 5 5 5 5 . . . . . 
+    . . . . 5 5 5 5 5 5 5 5 . . . . 
+    . . . . 5 5 5 f f 5 5 5 . . . . 
+    . . . . 5 5 f f f f 5 5 . . . . 
+    . . . . 5 5 f f f f 5 5 . . . . 
+    . . . . 5 5 f f f f 5 5 . . . . 
+    . . . . 5 5 f f f f 5 5 . . . . 
+    . . . . 5 5 f f f f 5 5 . . . . 
+    . . . . 5 5 f f f f 5 5 . . . . 
+    . . . . 5 5 f f f f 5 5 . . . . 
+    . . . . 5 5 5 f f 5 5 5 . . . . 
+    . . . . 5 5 5 5 5 5 5 5 . . . . 
+    . . . . . 5 5 5 5 5 5 . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
